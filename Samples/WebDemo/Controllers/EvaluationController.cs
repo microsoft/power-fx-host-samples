cc968c226d89346900dc9e9c137693efbef201af
jas-valgotar <jasvalgotar@microsoft.com>
2022-08-04 12:17:08 -0500

    Added timeout.

--- Samples/WebDemo/Controllers/EvaluationController.cs
+++ Samples/WebDemo/Controllers/EvaluationController.cs
@@ -1,55 +1,57 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.PowerFx;
 using Microsoft.PowerFx.Types;
 using PowerFxService.Model;
-using System;
 
 namespace PowerFxService.Controllers
 {
-    // Called by the demo page to evaluate an expression. 
+    // Called by the demo page to evaluate an expression.
     [ApiController]
     [Route("eval")]
     public class EvaluationController : ControllerBase
     {
         private readonly ILogger<EvaluationController> _logger;
-        
+        public static TimeSpan timeout = TimeSpan.FromSeconds(2);
+
         public EvaluationController(ILogger<EvaluationController> logger)
         {
             _logger = logger;
         }
 
-        // Passed  in from demo page to do an evaluation. 
+        // Passed  in from demo page to do an evaluation.
         public class RequestBody
         {
-            // additional symbols passed in as a json object. 
+            // additional symbols passed in as a json object.
             public string context { get; set; }
 
-            // the user's Power Fx expression to be evaluated 
+            // the user's Power Fx expression to be evaluated
             public string expression { get; set; }
         }
 
         [HttpPost]
-        public IActionResult HandleRequest([FromBody] RequestBody body)
+        public async Task<IActionResult> HandleRequestAsync([FromBody] RequestBody body)
         {
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(timeout);
             try
             {
                 var engine = new PowerFxScopeFactory().GetEngine();
-                
-                var parameters = (RecordValue) FormulaValue.FromJson(body.context);
-                
-                // TODO - this should be eval Async and catch timeouts. 
+
+                var parameters = (RecordValue)FormulaValue.FromJson(body.context);
+
                 if (parameters == null)
                 {
                     parameters = RecordValue.Empty();
                 }
-                var check = engine.Check(body.expression, parameters.Type, options:null);
-                check.ThrowOnErrors();
-                var result =  check.Expression.EvalAsync(parameters, cancel: CancellationToken.None).Result;
 
+                var result = await EvalAsync(engine, body.expression, cts.Token, parameters, options: null);
                 var resultString = PowerFxHelper.TestToString(result);
 
                 return StatusCode(200, new { result = resultString });
@@ -58,6 +60,21 @@ namespace PowerFxService.Controllers
             {
                 return StatusCode(200, new { error = ex.Message });
             }
+            finally
+            {
+                cts.Dispose();
+            }
+        }
+
+        private async Task<FormulaValue> EvalAsync(IPowerFxEngine engine, string expressionText, CancellationToken cancel, RecordValue parameters = null, ParserOptions options = null)
+        {
+            if (parameters == null)
+            {
+                parameters = RecordValue.Empty();
+            }
+            var check = engine.Check(expressionText, parameters.Type, options);
+            check.ThrowOnErrors();
+            return await check.Expression.EvalAsync(parameters, cancel);
         }
     }
 }
