385199745ed596b23c0ff5dff321adc04ce5b7bf
jas-valgotar <jasvalgotar@microsoft.com>
2023-10-03 14:04:13 -0400

    Adds DV101

---
+++ Samples/Dataverse101/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.PowerFx;
+using Microsoft.PowerFx.Dataverse;
+using Microsoft.PowerFx.Types;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using System.Configuration;
+
+namespace PowerFxHostSamples
+{
+    class Dataverse101
+    {
+        public static async Task Main()
+        {
+            string? connectionString = null;
+
+            while(connectionString == null)
+            {
+                Console.Write("Enter Dataverse Connection string: ");
+                connectionString = Console.ReadLine();
+            }
+
+            var svcClient = new ServiceClient(connectionString) { UseWebApi = false };
+            var dataverse = SingleOrgPolicy.New(svcClient);
+            var symbolValues = dataverse.SymbolValues;
+
+            var expressionText = "First(Accounts).'Account Name'";
+
+            var engine = new RecalcEngine();
+
+            var result = await engine.EvalAsync(expressionText, default, symbolValues);
+
+            var name = ((StringValue)result).Value;
+
+            Console.WriteLine($"{expressionText} -> {name}");
+        }
+    }
+}
