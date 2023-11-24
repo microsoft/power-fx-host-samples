e0a6b8c08a9467087ea34bc7c606c670e6169ab5
Dominik Downarowicz <dominik@downardo.at>
2023-11-24 15:40:11 +0000

    Add D365FO PowerFxDemo Model

---
+++ Samples/Dynamics365FODemo/registerSymbolicLink.ps1
@@ -0,0 +1,15 @@
+﻿$AOSMetadataPath = "K:\AOSService\PackagesLocalDirectory"
+
+If(!(Test-Path -Path $AOSMetadataPath))
+{
+      $AOSMetadataPath = "C:\AOSService\PackagesLocalDirectory"
+}
+
+$RepoPath = "."
+$RepoMetadataPath = $RepoPath + "\Metadata"
+$RepoModelFolders = Get-ChildItem $RepoMetadataPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	$Target = "$RepoMetadataPath\$ModelFolder"
+	New-Item -ItemType SymbolicLink -Path "$AOSMetadataPath" -Name "$ModelFolder" -Value "$Target"
+}
