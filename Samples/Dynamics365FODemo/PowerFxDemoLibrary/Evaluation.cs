0c30a571e1b808600ccefd26a7479a739fb73c40
Dominik Downarowicz <dominik@downardo.at>
2023-11-24 16:11:14 +0000

    Add PowerFx .Net Library Project (Example from: https://github.com/microsoft/power-fx-host-samples/tree/main/Samples/WebDemo)

---
+++ Samples/Dynamics365FODemo/PowerFxDemoLibrary/Evaluation.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.PowerFx;
+using Microsoft.PowerFx.Syntax;
+using Microsoft.PowerFx.Types;
+using System;
+using System.Collections.Generic;
+
+namespace PowerFxDemoLibrary
+{
+    public class Evaluation
+    {
+        public class Response
+        {
+            public string output { get; set; }
+            public string error { get; set; }
+            public bool   errorThrown { get; set; }
+        }
+
+        private class ThrowErrorFunction : ReflectionFunction
+        {
+            private string errorMessage = "";
+
+            public ThrowErrorFunction(): base("ThrowError", FormulaType.Blank, new[] { FormulaType.String }) { }
+
+            public BlankValue Execute(StringValue x)
+            {
+                addError(x.Value);
+                return FormulaValue.NewBlank();
+            }
+
+            public void addError(string error)
+            {
+                if (error != "")
+                {
+                    errorMessage += errorMessage + "\n" + error;
+                }
+                else
+                {
+                    errorMessage += errorMessage + "\n";
+                }
+            }
+
+            public string getErrorMessage()
+            {
+                return errorMessage.TrimStart('\n').TrimEnd('\n');
+            }
+
+            public bool isFailed()
+            {
+                return errorMessage != "" ? true : false;
+            }
+
+        }
+
+        private class ThrowErrorFunctionEmtpy : ReflectionFunction
+        {
+            public ThrowErrorFunction function;
+            public ThrowErrorFunctionEmtpy(ThrowErrorFunction _function) : base("ThrowError", FormulaType.Blank) 
+            {
+                function = _function;
+            }
+
+            public BlankValue Execute()
+            {
+                function.addError("");
+                return FormulaValue.NewBlank();
+            }
+        }
+
+        public Response EvaluateInput(string context, string expression)
+        {
+            Response response = new Response();
+            IReadOnlyList<Token> tokens = null;
+            CheckResult check = null;
+
+            ThrowErrorFunction errorFunction = new ThrowErrorFunction();
+            ThrowErrorFunctionEmtpy errorFunctionEmpty = new ThrowErrorFunctionEmtpy(errorFunction);
+
+            response.output = "";
+            response.error = "";
+            response.errorThrown = false;
+            try
+            {
+                var config = new PowerFxConfig();
+                config.AddFunction(errorFunction);
+                config.AddFunction(errorFunctionEmpty);
+                var engine = new RecalcEngine(config);
+
+                var parameters = (RecordValue)FormulaValueJSON.FromJson(context);
+
+                if (parameters == null)
+                {
+                    parameters = RecordValue.Empty();
+                }
+
+                tokens = engine.Tokenize(expression);
+                check = engine.Check(expression, parameters.Type, options: null);
+                check.ThrowOnErrors();
+                var eval = check.GetEvaluator();
+                var result = eval.Eval(parameters);
+
+                response.output = HelperClass.TestToString(result);
+            }
+            catch(Exception ex)
+            {
+                errorFunction.addError(ex.Message);
+            }
+            finally
+            {
+                response.errorThrown = errorFunction.isFailed();
+                response.error = errorFunction.getErrorMessage();
+            }
+
+            return response;
+
+        }
+
+    }
+
+}
---
+++ Samples/Dynamics365FODemo/PowerFxDemoLibrary/HelperClass.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.PowerFx.Types;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace PowerFxDemoLibrary
+{
+    // Helper to print results. 
+    // This should get shared with Repl or from framework.
+    public class HelperClass
+    {
+        internal static string TestToString(FormulaValue result)
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                TestToString(result, sb);
+            }
+            catch (Exception e)
+            {
+                // This will cause a diff and test failure below. 
+                sb.Append($"<exception writing result: {e.Message}>");
+            }
+
+            string actualStr = sb.ToString();
+            return actualStr;
+        }
+
+        // $$$ Move onto FormulaValue. 
+        // Result here should be a string value that could be parsed. 
+        // Normalize so we can use this in test cases. 
+        internal static void TestToString(FormulaValue result, StringBuilder sb)
+        {
+            if (result is DecimalValue dec)
+            {
+                sb.Append(dec.Value);
+            }
+            else if (result is NumberValue n)
+            {
+                sb.Append(n.Value);
+            }
+            else if (result is BooleanValue b)
+            {
+                sb.Append(b.Value ? "true" : "false");
+            }
+            else if (result is StringValue s)
+            {
+                // $$$ proper escaping?
+                sb.Append('"' + s.Value + '"');
+            }
+            else if (result is TableValue t)
+            {
+                var tableType = (TableType)t.Type;
+                var canUseSquareBracketSyntax = t.IsColumn && t.Rows.All(r => r.IsValue) && tableType.FieldNames.First() == "Value";
+                if (canUseSquareBracketSyntax)
+                {
+                    sb.Append('[');
+                }
+                else
+                {
+                    sb.Append("Table(");
+                }
+
+                var dil = string.Empty;
+                foreach (var row in t.Rows)
+                {
+                    sb.Append(dil);
+                    dil = ",";
+
+                    if (canUseSquareBracketSyntax)
+                    {
+                        var val = row.Value.Fields.First().Value;
+                        TestToString(val, sb);
+                    }
+                    else
+                    {
+                        if (row.IsValue)
+                        {
+                            TestToString(row.Value, sb);
+                        }
+                        else
+                        {
+                            TestToString(row.ToFormulaValue(), sb);
+                        }
+                    }
+
+                    dil = ",";
+                }
+
+                if (canUseSquareBracketSyntax)
+                {
+                    sb.Append(']');
+                }
+                else
+                {
+                    sb.Append(')');
+                }
+            }
+            else if (result is RecordValue r)
+            {
+                var fields = r.Fields.ToArray();
+                Array.Sort(fields, (a, c) => string.CompareOrdinal(a.Name, c.Name));
+
+                sb.Append('{');
+                var dil = string.Empty;
+
+                foreach (var field in fields)
+                {
+                    sb.Append(dil);
+                    sb.Append(field.Name);
+                    sb.Append(':');
+                    TestToString(field.Value, sb);
+
+                    dil = ",";
+                }
+
+                sb.Append('}');
+            }
+            else if (result is TimeValue tv)
+            {
+                sb.Append(tv.Value.ToString());
+            }
+            else if (result is BlankValue)
+            {
+                sb.Append("Blank()");
+            }
+            else if (result is DateValue d)
+            {
+                // Date(YYYY,MM,DD)
+                var date = d.GetConvertedValue(null);
+                sb.Append($"Date({date.Year},{date.Month},{date.Day})");
+            }
+            else if (result is DateTimeValue dt)
+            {
+                // DateTime(yyyy,MM,dd,HH,mm,ss,fff)
+                var dateTime = dt.GetConvertedValue(null);
+                sb.Append($"DateTime({dateTime.Year},{dateTime.Month},{dateTime.Day},{dateTime.Hour},{dateTime.Minute},{dateTime.Second},{dateTime.Millisecond})");
+            }
+            else if (result is ErrorValue)
+            {
+                sb.Append(result);
+            }
+            else
+            {
+                throw new InvalidOperationException($"unsupported value type: {result.GetType().Name}");
+            }
+        }
+    }
+}
