846085ab30b55c6a73dad116f7995b384c8d8313
Greg Lindhorst <gregli@microsoft.com>
2025-05-19 18:32:18 -0700

    more

---
+++ Samples/MCPServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using Microsoft.PowerFx.Core;
+using Microsoft.PowerFx.Repl;
+using Microsoft.PowerFx.Repl.Functions;
+using Microsoft.PowerFx.Repl.Services;
+using Microsoft.PowerFx.Types;
+using System.ComponentModel;
+using Microsoft.PowerFx;
+using System.Globalization;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class PowerFxTool
+{
+    [McpServerTool, Description("Evaluates a Power Fx formula and returns the result.")]
+    public static string Evaluate(string message)
+    {
+        var config = new PowerFxConfig();
+        var engine = new RecalcEngine(config);
+        var result = engine.Eval(message);
+        return result.ToExpression();
+    }
+}
